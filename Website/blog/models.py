845b11d02bab99a13e128f9b2d35b664be604952
Ali Fattahian <ali.fattahian1378@gmail.com>
2021-08-31 21:28:23 +0430

    add database models and use them instead of data from views

--- Website/blog/models.py
+++ Website/blog/models.py
@@ -1,3 +1,36 @@
+from django.core import validators
 from django.db import models
+from django.db.models.fields import CharField
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length= 80)
+    email_address = models.EmailField(null = True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+
+class Tag(models.Model):
+    caption = CharField(max_length= 50, null = True)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    excerpt = models.CharField(max_length= 200)
+    image_name = models.CharField(max_length= 50)
+    date = models.DateField(auto_now = True)
+    slug = models.SlugField(unique=True) #db_index = True is here by default, we don't have to add it.(it is use to make quering the data more efficient)
+    content = models.TextField(validators = [MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete= models.SET_NULL, related_name = 'posts', null = True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'{self.title}  |  {self.author}'
+
+
--- Website/blog/views.py
+++ Website/blog/views.py
@@ -1,83 +1,26 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Ali",
-    "date": date(2021, 8, 16),
-    "title": "Mountain Hiking",
-    "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-    "content": """
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    """
-    },
-    {
-    "slug": "programming-is-fun",
-    "image": "coding.jpg",
-    "author": "Ali",
-    "date": date(2021, 7, 16),
-    "title": "Programming Is Great!",
-    "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-    "content": """
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    """
-    },
-    {
-    "slug": "into-the-woods",
-    "image": "woods.jpg",
-    "author": "Ali",
-    "date": date(2021, 6, 16),
-    "title": "Nature At Its Best",
-    "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-    "content": """
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    """
-    }
-]
 
 def get_date(post):
     return post['date']
 
 def index(request):
-    sorted_posts = sorted(all_posts, key = get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
         'posts':latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         'all_posts':all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug = slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, 'blog/post-detail.html', {
-        'one_post' : identified_post
+        'one_post' : identified_post,
+        'post_tags':identified_post.tag.all()
     })
