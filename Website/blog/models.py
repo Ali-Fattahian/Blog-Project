bb1dbca31b66db33f4bdea4390e3962f0c470438
Ali Fattahian <ali.fattahian1378@gmail.com>
2021-09-29 02:28:02 +0330

    add read later feature for the website

--- Website/blog/models.py
+++ Website/blog/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag)
 
     def __str__(self):
-        return f'{self.title}  |  {self.author}'
+        return f'Post Title : {self.title}  |  Post Author : {self.author}'
 
 class Comment(models.Model):
     username = models.CharField(max_length=120)
--- Website/blog/urls.py
+++ Website/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.LatestPostsView.as_view(), name = 'homepage'), 
     path('posts', views.AllPostsView.as_view(), name = 'posts'),
-    path('posts/<slug:slug>', views.PostDetailView.as_view(), name = 'post')
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name = 'post'),
+    path('read-later', views.ReadLaterView.as_view(), name = 'read-later')
 ]
