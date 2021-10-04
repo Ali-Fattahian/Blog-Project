bcef68aed8951f9ad6d166d259c0749e3b3d29b4
Ali Fattahian <ali.fattahian1378@gmail.com>
2021-10-04 18:34:08 +0330

    add bootstrap for navbar and add flexbox for summary section

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -35,13 +35,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
 ]
 
 MIDDLEWARE = [
