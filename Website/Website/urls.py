a02603bd4cb65cedd55cb902febb725f0eb4e2bc
Ali Fattahian <ali.fattahian1378@gmail.com>
2021-11-03 01:15:09 +0330

    enable blog url

--- Website/Website/urls.py
+++ Website/Website/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('blog.urls')),
+    path('', include('blog.urls')),
     path('user/', include('users.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
